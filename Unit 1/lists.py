6047b191c7f885bd02577bb2f13dd0d37d238c0d
AbderrahimRezki <62284172+AbderrahimRezki@users.noreply.github.com>
2021-12-13 21:57:01 +0100

    Create lists.py

---
+++ Unit 1/lists.py
@@ -0,0 +1,5 @@
+ls = [i for i in range(1, 300) if i % 2 == 0]
+print("The length of the list: ", len(ls))
+for number in ls:
+  print(f"The square of the {number} is: {number * number}")
+print("Checking if 57 in ls", 57 in ls)
